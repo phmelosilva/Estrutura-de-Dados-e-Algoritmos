e12324b4d50ab807de322d74221342c9250599a4
Pedro Henrique <pedrodsm1819@gmai.com>
2022-11-03 22:30:09 -0300

    Tentando exercicio da Viagem

---
+++ Exercícios_Lista1/Exercicio_D3/Viagem_aconselha.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int dia_viagem, R, Ri, i, codigo_atual, nota_atual, maior_nota, maior_codigo;
+    int i_restaurantes, codigos[10000000], notas[5];
+    
+    while (scanf("%d", &R) != EOF) {
+        for (i=0; i < R*2; i++) {
+            scanf("%d %d", codigo_atual, nota_atual);
+            // Preciso pegar os códigos e notas atuais, e comparar eles em variáveis que compara as notas.
+        }
+        for (i=0; i < R*2; i++) {
+            if () {
+                
+            }
+        }
+    }
+    return 0;
+}
