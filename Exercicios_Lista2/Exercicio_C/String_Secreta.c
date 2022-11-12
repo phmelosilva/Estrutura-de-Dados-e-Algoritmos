a07263e156caf22299d1274c4842cad9170166d7
Pedro Henrique <pedrodsm1819@gmai.com>
2022-11-12 11:52:49 -0300

    Exercício String Secreta

--- Exercicios_Lista2/Exercicio_C/String_Secreta.c
+++ Exercicios_Lista2/Exercicio_C/String_Secreta.c
@@ -1,25 +1,18 @@
 #include <stdio.h>
-#include <string.h> 
 
 int main() {
     int i;
-    long num;
-    scanf("%lx", &num);
-    // 7F -> maior
-    // while ( != EOF) {
-        
-        // for (i=0; i <255; i++) {
-        //     if (num == i) {
-        //         printf("%c", i);
-        //     }
-        // }
+    int num;
+    while ( scanf("%x", &num) != EOF) {
+        char *valor = &num;
+        for (i=0; i < 4; i++) {
+            if (valor[i] == 0) {
+                printf("\n");
+                return 0;
+            }
+            printf("%c",(char)valor[i]);
+        }   
         // 61 + 6c + 4f
         // A + L + O
-    
-    
-    printf("%x\n", num);
-    printf("%d\n", num);
-    printf("%s\n", num);
-
-    return 0;
-}
+    }
+};
