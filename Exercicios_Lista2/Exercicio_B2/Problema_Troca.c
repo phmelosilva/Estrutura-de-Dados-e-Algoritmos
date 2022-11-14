46bf0b11db8eaed6ac64dc05a41d4173c431e792
Pedro Henrique <pedrodsm1819@gmai.com>
2022-11-14 14:57:05 -0300

    Correção no exercício de troca de valores

--- Exercicios_Lista2/Exercicio_B2/Problema_Troca.c
+++ Exercicios_Lista2/Exercicio_B2/Problema_Troca.c
@@ -1,8 +1,18 @@
 void swap(int *a, int *b)
 {
-    int *interm = a;
-    a = b;
-    b = interm;
-    printf("pontA:%d\n",a);
-    printf("pontB:%d\n",b);
+    int interm = *a;
+    *a = *b;
+    *b = interm;
+    // printf("pontA:%d\n",*a);
+    // printf("pontB:%d\n",*b);
 }
+
+// int main() {
+//     int n1, n2;
+//     scanf("%d%d", &n1, &n2);
+//     printf("pontA: %d\n", n1);
+//     printf("pontB: %d\n", n2);
+//     printf("\n\n");
+//     swap(&n1, &n2);
+//     return 0;
+// }
